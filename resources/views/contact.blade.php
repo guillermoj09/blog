de6576310bf40a6284ff6df6e35f1598e08c0915
rckdiaz <67074104+rckdiaz@users.noreply.github.com>
2020-11-21 00:26:51 -0300

    otro

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,21 +4,20 @@
     <div class="row">
     <div class="col-lg-9">
         <div class="contact-box">
-            <a class="row" href="profile.html">
+            <a class="row" href="#">
             <div class="col-4">
                 <div class="text-center">
-                    <img alt="image" class="rounded-circle m-t-xs img-fluid" src="img/a2.jpg">
-                    <div class="m-t-xs font-bold">Graphics designer</div>
+                    <img alt="image" class="rounded-circle m-t-xs img-fluid" src="img/foto.jpg" height="250" width="250">
+                    <div class="m-t-xs font-bold">Candidata Presidencial</div>
                 </div>
             </div>
             <div class="col-8">
-                <h3><strong>John Smith</strong></h3>
-                <p><i class="fa fa-map-marker"></i> Riviera State 32/106</p>
+                <h3><strong>Geraldine Alarcón Rojas</strong></h3>
+                <p><i class="fa fa-map-marker"></i> Arica, Chile</p>
                 <address>
                     <strong>Twitter, Inc.</strong><br>
-                    795 Folsom Ave, Suite 600<br>
-                    San Francisco, CA 94107<br>
-                    <abbr title="Phone">P:</abbr> (123) 456-7890
+                        Codpa, #2882<br>
+                    <abbr title="Phone">Teléfono:</abbr> (+56) 9 5673-7076
                 </address>
             </div>
                 </a>
